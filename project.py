c391ccd83ef16a556850eade24da002657680de4
caloloc2 <calolomino@gmail.com>
2021-02-01 23:10:33 -0500

    actualizacion

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # prob = [0.5, 0.5]
 # prob = [0.9, 0.1]
 
-iteraciones = 4000
+iteraciones = 2000
 n0 = WalkRandom(p=0.7, iteraciones=iteraciones)
 n1 = WalkRandom(p=0.5, iteraciones=iteraciones)
 n2 = WalkRandom(p=0.1, iteraciones=iteraciones)
@@ -18,14 +18,17 @@ val2 = 0
 for i in range(iteraciones):
     val0 = n0.analiza(i, val2)
     val1 = n1.analiza(i, val0)
-    val2 = n2.analiza(i, val1)    
+    val2 = n2.analiza(i, val1)
 
 walk0 = n0.obtener_caminata()
 walk1 = n1.obtener_caminata()
 walk2 = n2.obtener_caminata()
 
-fig, axs = plt.subplots(3)
+fig, axs = plt.subplots(4)
 axs[0].plot(walk0, label = "n0", color="green")
 axs[1].plot(walk1, label = "n1", color="red")
 axs[2].plot(walk2, label = "n2", color="blue")
+axs[3].plot(walk0, label = "n0", color="green")
+axs[3].plot(walk1, label = "n1", color="red")
+axs[3].plot(walk2, label = "n2", color="blue")
 plt.show()
--- walk_random.py
+++ walk_random.py
@@ -12,7 +12,9 @@ class WalkRandom():
         self.__g = g
         self.__posiciones = np.zeros(iteraciones) ## [0] # posiciones con inicio en cero
         self.__posicion_actual = 0
+        self.__pf_sel = [0, 4]
         self.__iteraciones = iteraciones
+        self.__sumatoria = 0
         self.__randomicos = self.__genera()
     
     def __genera(self):
@@ -24,25 +26,57 @@ class WalkRandom():
         valores = self.__randomicos[0][pos]
         probabilidad = self.__randomicos[1][pos]
 
+        if self.__pf_sel[0] == 1:            
+            self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L
+            self.__pf_sel[1] -= 1
+
+            if (self.__pf_sel[1] == 0):
+                self.__pf_sel = [0, 4]
+        elif self.__pf_sel[0] == -1:            
+            self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+            self.__pf_sel[1] -= 1
+
+            if (self.__pf_sel[1] == 0):
+                self.__pf_sel = [0, 4]
+        elif self.__pf_sel[0] == 0:
+            self.__sumatoria = self.__posiciones[pos - 1] + vecino
+            factor = self.__g * self.__sumatoria
+            # print(self.__posiciones[pos - 1], vecino, factor)
+            if (self.__posiciones[pos - 1] < self.__L): # Si el valor es menor a L
+                if probabilidad:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] + self.__C) # Aumenta valor en C
+                else:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1]) # Mantiene el mismo valor
+            else:
+                if probabilidad:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] + (3*self.__L)) # Sube a 3L
+                    self.__pf_sel[0] = 1
+                else:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+                    self.__pf_sel[0] = -1  
+
+        return self.__posiciones[pos]    
+
+    def analiza2(self, pos, vecino):
+        valores = self.__randomicos[0][pos]
+        probabilidad = self.__randomicos[1][pos]
+
         if probabilidad:
             if self.__posiciones[pos-1] < self.__L:
                 self.__posiciones[pos] = (self.__posiciones[pos-1] + self.__C) # Aumenta el valor en C
-            else:
-                if self.__posiciones[pos-1] == (3*self.__L):
-                    
-                else: 
-                    if valores > self.__pf:
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] + (3*self.__L)) # Sube a 3L
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L
-                    else:
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
-                        self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+            else:               
+                if valores > self.__pf:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] + (3*self.__L)) # Sube a 3L
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L     
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - self.__L) # Decrece en 1L
+                else:
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
+                    self.__posiciones[pos] = (self.__posiciones[pos-1] - (self.__L/5)) # Decrece en L/5
         else:
             if self.__posiciones[pos-1] < self.__L:
                 self.__posiciones[pos] = (self.__posiciones[pos-1]) # Se mantiene en el mismo valor
